1d095f96387dc36949e3281550449abaf7493d98
Decatalyst247 <seidoavoseh24@gmail.com>
2026-08-07 21:24:23 +0100

    Fix allowed host for render

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-l^3jz9q=hgj1qgnl!b((qfcup!-^$&2wfat6=nvxy_$llkxc)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",
-"localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
