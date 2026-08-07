7c021c38c791fbe4e2cc2db9167c680cd3f8fd1b
Decatalyst247 <seidoavoseh24@gmail.com>
2026-08-07 22:21:55 +0100

    fix debug to false

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l^3jz9q=hgj1qgnl!b((qfcup!-^$&2wfat6=nvxy_$llkxc)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ogulanguagebank.com","www.ogulanguagebank.com",]
 
 
 # Application definition
